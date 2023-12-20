ec284237e75df40ecbbc1104e20d8797cd287669
vladolaru <vlad.olaru@arnia.ro>
2023-12-20 15:30:08 +0200

    snowflakes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import Snowflakes from "magic-snowflakes";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pandoras-food';
+
+  snowflakes = new Snowflakes();
+
+  ngOnInit(): void {
+    this.snowflakes.start();
+  }
+
+  ngOnDestroy(): void {
+    this.snowflakes.stop();
+  }
 }
